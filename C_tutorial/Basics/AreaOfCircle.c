ae7dd3420bf7a4bf30d0bf7ab7e8a8b81a5d547e
Karan-Palan <karanpalan007@gmail.com>
2024-01-23 19:49:49 +0530

    Chapter1-Variables and Dtypes done

---
+++ C_tutorial/Basics/AreaOfCircle.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main  () {
+    float radius;
+    printf("enter radius : ");
+    scanf("%f", &radius);
+    printf("The radius is :%f", radius*3.14*radius);
+}
---
+++ C_tutorial/Basics/DataTypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+// This is a comment in C
+/* 
+This is a multiline 
+comment
+*/
+int main() {
+int age = 22;
+float pi = 3.14;
+char percentage = '%';
+return 0;
+}
